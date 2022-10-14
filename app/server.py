a8690290fb9530aaa8151573b8c6b099f588ec19
Agam <swagamjot@gmail.com>
2022-10-14 02:22:55 -0400

    Rough Demo Version of app completed

--- app/server.py
+++ app/server.py
@@ -24,7 +24,7 @@ def Calculate():
         discount = float(request.form.get("discount"))
         tax = float(request.form.get("tax"))
 
-        item_cost_before_tax = quantity*round(( price - (price)*(discount/100) ), 2 )
+        item_cost_before_tax = round(quantity*round(( price - (price)*(discount/100) ), 2 ), 2)
         item_cost_after_tax = round(item_cost_before_tax * (1 + (tax/100)), 2)
 
         # global before_tax
@@ -61,29 +61,42 @@ def Calculate():
 
         # item_list.append(before_tax)
         # item_list.append(after_tax)
+        return render_template('checkout.html', info=item_list, total_bill=(round(before_tax, 2), round(after_tax, 2)))
 
     # item_list_index = len(item_list)
 
-    # if i >= 5:
-    return render_template('checkout.html', info=item_list, total_bill=(before_tax, after_tax))
+    return render_template('checkout.html', info=item_list, total_bill=(round(before_tax, 2), round(after_tax, 2)))
     # else:
     #     return render_template('checkout.html')
 
-# @app.route('/remove', methods=['POST', 'GET'])
-# def removeItem(i):
-#     bt = item_list[i].quantity*round(( item_list[i].price - (item_list[i].price)*(item_list[i].discount/100) ), 2 )
-#     at = round(bt * (1 + (item_list[i].tax/100)), 2)
-#     before_tax -= bt
-#     aftertax -= at
-#     item_list.pop(i)
+@app.route('/remove', methods=['POST', 'GET'])
+def removeItem(i):
+    if request.method=='POST' and request.form:
+        global item_list
+        bt = item_list[i].quantity*round(( item_list[i].price - (item_list[i].price)*(item_list[i].discount/100) ), 2 )
+        at = round(bt * (1 + (item_list[i].tax/100)), 2)
+        global before_tax
+        before_tax -= bt
+        global after_tax
+        after_tax -= at
+        item_list.pop(i)
+    return render_template('checkout.html', total_bill=(before_tax, after_tax), info=item_list)
+
     
 #     return render_template('checkout.html', info=item_list, total_bill=(before_tax, after_tax))
-# @app.route('/total', methods=['POST', 'GET'])
-# def calculateTotal():
-#     # total = []
-#     # total.append(before_tax)
-#     # total.append(after_tax)
-#     return render_template('checkout.html', total_bill=(before_tax, after_tax), info=item_list)
+@app.route('/reset', methods=['POST', 'GET'])
+def clear():
+    global item_list
+    item_list = []
+
+    global before_tax
+    before_tax = 0.0
+    global after_tax 
+    after_tax = 0.0
+
+    global i
+    i = 0
+    return render_template('checkout.html', total_bill=(before_tax, after_tax), info=item_list)
 
 
 
